3d1840f0c9df1d20851cdeed0eb864e86db89782
Ehasanul Haque <ehasanmd365@gmail.com>
2026-05-23 18:03:32 +0600

    get/post added

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 
 app.use(morgan('dev')); //will run later than the custom middleware, because it is placed after the custom middleware in the code.
 
+//re.body te data pawar jnno ei 2 ta use krte hbe, jodi json data pathano hoy tahole express.json() use krte hbe, jodi form data pathano hoy tahole express.urlencoded() use krte hbe.
+app.use(express.json()); //body parser, json data parse kore req.body te rakhbe, for example: { "username": "john", "email": "
+app.use(express.urlencoded({ extended: true })); //form data parse kore req.body te rakhbe, for example: { "username": "john", "email": "john@example.com" }
+
 app.set('view engine', 'ejs'); //set method use kore view engine set kora hoyeche, ejs use korar jonno
 
 app.use((req, res, next) => {  //always put middleware before all routes
@@ -37,6 +41,16 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.send('contact');
+});
+
+app.get('/form-get', (req, res) => { //het method e paraemtre e all data dkhabe
+  console.log(req.query); //query parameter dekhabe, for example: /form-get?username=john&email=john@example.com
+  res.send('data received');
+});
+
+app.post('/form-post', (req, res) => { //post method e body e data dkhabe
+  console.log(req.body); //body parameter dekhabe, for example: /form-post with a POST request containing JSON data
+  res.send('data received');
 });
 
 app.listen(3000);
